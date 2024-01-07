663e1a0d50cafec5749c7f450bec8a95a8963839
ovo <1060037850@qq.com>
2024-01-07 22:42:37 +0800

    完善ui文件。

---
+++ WeatherTray/WeatherTray.cpp
@@ -0,0 +1,32 @@
+#include "WeatherTray.h"
+
+WeatherTray::WeatherTray(QWidget* parent)
+    : QMainWindow(parent)
+{
+    ui.setupUi(this);
+
+    setWindowFlag(Qt::FramelessWindowHint);          //设置窗口无边框
+    setFixedSize(width(), height());
+
+    mExitMenu = new QMenu(this);
+    mExitAct = new QAction();
+
+    mExitAct->setText("退出Exit");
+    mExitAct->setIcon(QIcon(":/res/close.png"));
+
+    mExitMenu->addAction(mExitAct);
+
+    connect(mExitAct, &QAction::triggered, this, [=] {
+        qApp->exit(0);
+        });
+}
+
+WeatherTray::~WeatherTray()
+{}
+
+void WeatherTray::contextMenuEvent(QContextMenuEvent* event)          //弹出右键菜单
+{
+    mExitMenu->exec(QCursor::pos());
+
+    //event->accept();
+}
---
+++ WeatherTray/main.cpp
@@ -0,0 +1,10 @@
+#include "WeatherTray.h"
+#include <QtWidgets/QApplication>
+
+int main(int argc, char *argv[])
+{
+    QApplication a(argc, argv);
+    WeatherTray w;
+    w.show();
+    return a.exec();
+}
